2654a48378567df1b2ba574b2e2aded223913649
Vsevolod Ryzhov <v.ryzhov@plein.com>
2026-03-27 17:50:13 +0300

    wip: fixes after code review

--- internal/config/options.go
+++ internal/config/options.go
@@ -14,7 +14,7 @@ var Options struct {
 func ParseFlags() {
 	flag.StringVar(&Options.AppPort, "a", "localhost:8080", "The address to bind the app to")
 	flag.StringVar(&Options.DatabaseDSN, "d", "", "Database connection string")
-	flag.StringVar(&Options.JWTSecretKey, "j", "your-256-bit-secret-key-change-in-production", "JWT secret key")
+	flag.StringVar(&Options.JWTSecretKey, "j", "", "JWT secret key")
 	flag.Parse()
 
 	if envRunAddr, exists := os.LookupEnv("SERVER_ADDRESS"); exists {
